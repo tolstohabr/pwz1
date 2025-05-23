9e6d62d84ffd7abcbc84aac2b98b90a3916b123c
Temirlan Ragimov <temirlan.official@mail.ru>
2025-05-23 19:44:37 +0300

    second commit

--- main.go
+++ main.go
@@ -38,6 +38,8 @@ func main() {
 			handleAcceptOrder(storage, args[1:])
 		case "return-order":
 			handleReturnOrder(storage, args[1:])
+		case "process-order":
+			handleProcessOrders(storage, args[1:])
 		default:
 			fmt.Println("Неизвестная команда. Введите 'help' для списка.")
 		}
@@ -49,6 +51,7 @@ func printHelp() {
 	fmt.Println("  help             Показать эту справку")
 	fmt.Println("  accept-order     Принять заказ от курьера")
 	fmt.Println("  return-order     Вернуть заказ") //выбросить на помойку
+	fmt.Println("  process-order    Выдать или принять возврат")
 	fmt.Println("  exit             Выйти из приложения")
 }
 
@@ -114,5 +117,41 @@ func handleReturnOrder(storage Storage, args []string) {
 		fmt.Println("ERROR:", err.Error())
 	} else {
 		fmt.Println("ORDER_RETURNED:", orderID)
+	}
+}
+
+func handleProcessOrders(storage Storage, args []string) {
+	var userID, action, orderIDsStr string
+
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "--user-id":
+			if i+1 < len(args) {
+				userID = args[i+1]
+				i++
+			}
+		case "--action":
+			if i+1 < len(args) {
+				action = args[i+1]
+				i++
+			}
+		case "--order-ids":
+			if i+1 < len(args) {
+				orderIDsStr = args[i+1]
+				i++
+			}
+		}
+	}
+
+	if userID == "" || action == "" || orderIDsStr == "" {
+		fmt.Println("ERROR: VALIDATION_FAILED: Требуются --user-id, --action и --order-ids")
+		return
+	}
+
+	orderIDs := strings.Split(orderIDsStr, ",")
+	results := ProcessOrders(storage, userID, action, orderIDs)
+
+	for _, res := range results {
+		fmt.Println(res)
 	}
 }
